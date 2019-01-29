f7879b0e06274711962f7235d63d4b08ad78ebe5
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-01-29 14:06:01 -0500

    created an intellij project

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,24 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int firstNumber,secondNumber;
+        String firstWord;
+        String secondWord;
+
+        firstWord="Grass";
+        secondWord="hopper";
+
+        System.out.println(firstWord+secondWord);
+        
+
+
+
+
+
+
+
+
+    }
+}
